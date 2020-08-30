c6363e3de844c50316e73b7fbf55e3de3b30a261
VeeKay1998 <venkatkoushik1998@gmail.com>
2020-08-30 21:37:03 +0530

    R script

---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+#Accessing data from unzipped UCI HAR Dataset folder....
+x.train <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\train\\X_train.txt")
+x.test <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\test\\X_test.txt")
+sub.train <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\train\\subject_train.txt")
+sub.test <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\test\\subject_test.txt")
+y.train <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\train\\Y_train.txt")
+y.test <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\test\\Y_test.txt")
+var.names <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\features.txt")
+#Merging of accessed data and assiginment of variablenames
+x <- rbind(x.train, x.test) #Here I have merged x_train and x_test datasets.
+names(x) <- var.names[,2] #Here I have assigined names to the variables from data given in features.
+y <- rbind(y.train, y.test) #Here I merged y_train and y_test datasets.
+names(y) <- "act_labels" 
+sub <- rbind(sub.train, sub.test)
+names(sub) <- "subject_id"
+#Extraction of measurements on mean and standed deviation from x.
+mean.index <- grep("mean()", var.names[,2]) #Here I have obtained the index aka. locations where mean() is present.
+std.index <- grep("std()", var.names[,2]) #Here I have obtained the index aka. locations where std() is present.
+x.mean.std <- x[,c(mean.index, std.index)] #Here I extract mean and std variables. 
+x.mean.std <- cbind(y, x.mean.std)
+act.labels <- read.table("C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\activity_labels.txt")
+for (val in 1: length(x.mean.std$act_labels)) { #Here using this for loop I replace Activity label with Activity name.
+  k <- x.mean.std$act_labels[val]
+  x.mean.std$act_labels[val] <- act.labels[k,2]
+}
+x.mean.std <- cbind(sub,x.mean.std) # Here I join Subject_id col. with x.mean.std.
+library(dplyr) #Here I import dplyr package.
+#Average of each variable for each activity and each subject.
+tidyset <- x.mean.std %>% group_by(subject_id, act_labels) %>% summarise_all(mean)# tidy set
+write.table(tidyset, "C:\\Users\\Venkat koushik\\R\\UCI HAR Dataset\\tidyset.txt", row.name=FALSE)writind ttidy set to text file. 
+
